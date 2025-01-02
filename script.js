b4a5cc11447104b96b1efa60536b0ad0169ba602
VASANTH KUMAR D <mybusiness.vkrs@gmail.com>
2025-01-02 13:34:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelector(".abt-me").addEventListener("click", () => {
+  window.location.href = "Home_page.html#aboutme";
+});
+
+document.querySelector(".proj").addEventListener("click", () => {
+  window.location.href = "Home_page.html#section_3";
+});
+
+document.querySelector(".blog").addEventListener("click", () => {
+  window.location.href = "Home_page.html#section_4";
+});
+
+document.querySelector(".cont").addEventListener("click", () => {
+  window.location.href = "Home_page.html#contact";
+});
