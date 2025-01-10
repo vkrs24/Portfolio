976a5aac2c5b2c4670111220affdf93767308a59
Vasanth Kumar <mybusiness.vkrs@gmail.com>
2025-01-10 09:40:11 +0530

    view more page is linked with main page

--- view_data.js
+++ view_data.js
@@ -6,22 +6,10 @@ const view_data = [
     desc: "This is one of my favourite project that it helps me to understand HTML5 and CSS with responsive design.",
   },
   {
-    image: "images/prime.png",
+    image: "images/amazon.png",
     link: "",
-    title: "Amazon Prime Clone Website",
-    desc: "This project is an intreasting one that it helps me to understand the javascript validations during Login and SignUp.",
-  },
-  {
-    image: "images/University.png",
-    link: "",
-    title: "University Portal Website",
-    desc: "This project provides me the basic understanding of HTML5 and CSS with responsive design for a website.",
-  },
-  {
-    image: "images/youtube.png",
-    link: "",
-    title: "YouTube Clone Website",
-    desc: "This is one of my favourite project that it helps me to understand HTML5 and CSS with responsive design.",
+    title: "Amazon ECommerce Website",
+    desc: "This project helps me to understand the javascript concepts that is required for a basic websites to generate the content dynamically and making webpages intreactive.",
   },
   {
     image: "images/prime.png",
@@ -34,6 +22,18 @@ const view_data = [
     link: "",
     title: "University Portal Website",
     desc: "This project provides me the basic understanding of HTML5 and CSS with responsive design for a website.",
+  },
+  {
+    image: "images/rps.png",
+    link: "",
+    title: "Rock Paper Scissors Game",
+    desc: "This one helps me to understand the frontend technologies like Javascript, HTML5 and CSS with responsive design.",
+  },
+  {
+    image: "images/todo.png",
+    link: "",
+    title: "Todo List Webpage",
+    desc: "It provides me understanding of how to create and delete an item in a webpage.",
   },
 ];
 
