5fe05c94813742b6dd9e50ec8db722170a904bb5
Vasanth Kumar <mybusiness.vkrs@gmail.com>
2025-01-10 09:40:11 +0530

    view more page is linked with main page

--- view_data.js
+++ view_data.js
@@ -1,39 +1,39 @@
 const view_data = [
   {
     image: "images/youtube.png",
-    link: "",
+    link: "https://vkrs24.github.io/YouTube-Clone/youtube.html",
     title: "YouTube Clone Website",
     desc: "This is one of my favourite project that it helps me to understand HTML5 and CSS with responsive design.",
   },
   {
+    image: "images/amazon.png",
+    link: "https://vkrs24.github.io/Amazon-e-commerce-Website/amazon.html",
+    title: "Amazon ECommerce Website",
+    desc: "This project helps me to understand the javascript concepts that is required for a basic websites to generate the content dynamically and making webpages intreactive.",
+  },
+  {
     image: "images/prime.png",
-    link: "",
+    link: "https://vkrs24.github.io/Amazon_PimeVideo_Clone/",
     title: "Amazon Prime Clone Website",
     desc: "This project is an intreasting one that it helps me to understand the javascript validations during Login and SignUp.",
   },
   {
     image: "images/University.png",
-    link: "",
+    link: "https://vkrs24.github.io/University_website/University_Website/index.html",
     title: "University Portal Website",
     desc: "This project provides me the basic understanding of HTML5 and CSS with responsive design for a website.",
   },
   {
-    image: "images/youtube.png",
-    link: "",
-    title: "YouTube Clone Website",
-    desc: "This is one of my favourite project that it helps me to understand HTML5 and CSS with responsive design.",
+    image: "images/rps.png",
+    link: "https://vkrs24.github.io/Rock_Paper_Scissors_Game/",
+    title: "Rock Paper Scissors Game",
+    desc: "This one helps me to understand the frontend technologies like Javascript, HTML5 and CSS with responsive design.",
   },
   {
-    image: "images/prime.png",
+    image: "images/todo.png",
     link: "",
-    title: "Amazon Prime Clone Website",
-    desc: "This project is an intreasting one that it helps me to understand the javascript validations during Login and SignUp.",
-  },
-  {
-    image: "images/University.png",
-    link: "",
-    title: "University Portal Website",
-    desc: "This project provides me the basic understanding of HTML5 and CSS with responsive design for a website.",
+    title: "Todo List Webpage",
+    desc: "It provides me understanding of how to create and delete an item in a webpage.",
   },
 ];
 
